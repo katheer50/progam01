dc226bb957c36b4605295290dc706db1c4d24de2
unknown <leop3f24@gmail.com>
2017-09-26 23:50:39 +0800

    pratice

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+	System.out.print("資傳系");
+	System.out.println("\n陳亦禹");
+	System.out.print("106024050" );
+	System.out.print("\"\n\\");
+
+    }
+}
